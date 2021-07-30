5f9066066d6ff6a78c950d0c8485cbc902153067
chhan <chhan@wdfall.com>
2021-07-30 13:26:51 +0900

    [update] content

--- src/main/java/ga/mr838wind/checkNovel/check/CheckService.java
+++ src/main/java/ga/mr838wind/checkNovel/check/CheckService.java
@@ -28,38 +28,19 @@ public class CheckService {
     private List<NovelVO> initNovelCheckList() {
         List<NovelVO> checkList = new ArrayList<>();
 
-        //
-        NovelVO vo = new NovelVO(1, "武谪仙",
-                "https://m.230book.com/book/14876/",
-                EL_SELECTOR_230BOOK);
-        checkList.add(vo);
+        checkList.add(new NovelVO(1, "仙狐",
+                "https://m.230book.com/book/26029/",
+                EL_SELECTOR_230BOOK
+        ));
 
-        NovelVO vo2 = new NovelVO(2, "武谪仙",
-                "http://m.biqugexs.com/34_34282/",
-                EL_SELECTOR_BIQUGEXS);
-        checkList.add(vo2);
-
-        //
-        NovelVO vo3 = new NovelVO(3, "天阿降临",
+        checkList.add(new NovelVO(2, "天阿降临",
                 "https://m.230book.com/book/154/",
-                EL_SELECTOR_230BOOK);
-        checkList.add(vo3);
+                EL_SELECTOR_230BOOK));
 
-        NovelVO vo4 = new NovelVO(4, "天阿降临",
-                "http://m.biqugexs.com/12_12592/",
-                EL_SELECTOR_BIQUGEXS);
-        checkList.add(vo4);
+        checkList.add(new NovelVO(3, "基因大时代",
+                "https://m.230book.com/book/16656/",
+                EL_SELECTOR_230BOOK));
 
-        //
-        NovelVO vo5 = new NovelVO(5, "沧元图",
-                "https://m.230book.com/book/2097/",
-                EL_SELECTOR_230BOOK);
-        checkList.add(vo5);
-
-        NovelVO vo6 = new NovelVO(6, "沧元图",
-                "http://m.biqugexs.com/22_22904/",
-                EL_SELECTOR_BIQUGEXS);
-        checkList.add(vo6);
 
         return checkList;
     }
